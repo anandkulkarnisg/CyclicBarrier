7b04a3499cbc0f0cac33467bf2beb7f156a9fc27
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2020-08-30 11:38:37 +0800

    Anand Kulkarni - 30/08/2020 11:38:36:848 Asia/Singapore(+08,+0800) - commited via autocommit tool.

--- Example7.cpp
+++ Example7.cpp
@@ -8,23 +8,19 @@
 
 using namespace std;
 
-long result=0;
-tuple<int, int> var=make_tuple(0, 0);
-CyclicBarrier preBarrier(2), postBarrier(2);
-
-void computeDotProduct(const vector<int>& v, const int& threadId)
+void computeDotProduct(const vector<int>& v, const int& threadId, long& result, tuple<int, int>& var, CyclicBarrier& preBarrier, CyclicBarrier& postBarrier)
 {
 	size_t size=v.size();
 	size_t startCounter=0;
-	while(startCounter<v.size())
+	while(startCounter<size)
 	{
 		int val=v[startCounter];
-		int preBrrierCount=preBarrier.await();
+		preBarrier.await();
 		if(threadId)
 			get<0>(var)=val;
 		else
 			get<1>(var)=val;
-		int postBarrierCount=postBarrier.await();
+		postBarrier.await();
 		if(!threadId)
 			result+=get<0>(var)*get<1>(var);
 		++startCounter;
@@ -33,14 +29,18 @@ void computeDotProduct(const vector<int>& v, const int& threadId)
 
 long getDotProduct(const vector<int>& v1, const vector<int>& v2)
 {
-	thread t1(&computeDotProduct, cref(v1), 0);
-	thread t2(&computeDotProduct, cref(v2), 1);
+  long result=0;
+  tuple<int, int> var=make_tuple(0, 0);
+  short barrierPair=2; // We need a pair of barriers [ i.e two of them ].
+  CyclicBarrier preBarrier(barrierPair), postBarrier(barrierPair);
+	thread t1(&computeDotProduct, cref(v1), 0, ref(result), ref(var), ref(preBarrier), ref(postBarrier));
+	thread t2(&computeDotProduct, cref(v2), 1, ref(result), ref(var), ref(preBarrier), ref(postBarrier));
 	t1.join();
 	t2.join();
 	return(result);
 }
 
-// fb interview questio : Implement a multi threaded sparse array dot product generator. The arrays are coming in from different sources, they are very large.
+// fb interview question : Implement a multi threaded sparse array dot product generator. The arrays are coming in from different sources, they are very large.
 // They are being read one index at a time from resource to save down on memory. Provide an efficient implementation of dot product. 
 // Time Complexity : O(n) , space complexity:O(1)
 // solved using Cyclic Barrier.
